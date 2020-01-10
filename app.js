b84b66211f73bf47a81727d633762bc27c2817d3
FrijkeUbuntu <FrijkeUbuntu@hotmail.com>
2020-01-10 16:31:18 +0100

    Styled the form

--- app.js
+++ app.js
@@ -47,8 +47,18 @@ app.get("/blogs/new", function(req, res){
     res.render("new");
 });
 
-
 // CREATE ROUTE
+app.post("/blogs", function(req, res){
+    // create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        } else {
+        // then, redirect to the index
+            res.redirect("/blogs");
+        }
+    });
+});
 
 app.listen(3002, function(){
     console.log( "The REST Server has started!!");
