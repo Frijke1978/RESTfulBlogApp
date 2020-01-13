1fd5560a35895db7374388845d755478ce7869f1
FrijkeUbuntu <FrijkeUbuntu@hotmail.com>
2020-01-13 13:28:41 +0100

    Fancyfied the RESTful Blog and added sanitizer

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var bodyParser = require("body-parser"),
-methodOverride = require("method-override"),
-mongoose       = require("mongoose"),
-express        = require("express"),
-app            = express();
+var bodyParser   = require("body-parser"),
+methodOverride   = require("method-override"),
+expressSanitizer = require("express-sanitizer"),
+mongoose         = require("mongoose"),
+express          = require("express"),
+app              = express();
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
@@ -10,6 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -52,6 +54,7 @@ app.get("/blogs/new", function(req, res){
 // CREATE ROUTE
 app.post("/blogs", function(req, res){
     // create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body)
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
@@ -91,6 +94,7 @@ app.get("/blogs/:id/edit", function(req, res){
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body)
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if(err){
             res.redirect("/blogs");
@@ -98,6 +102,19 @@ app.put("/blogs/:id", function(req, res){
             res.redirect("/blogs/" + req.params.id);
         }
     });
+});
+
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    // destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    })
+    // redirect somewhere
 });
 
 app.listen(3002, function(){
